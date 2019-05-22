861eab4ea7cf787e370491bcc51155531dc75350
deesnow <janos.tarjanyi@gmail.com>
2019-05-22 20:21:10 +0200

    bg task fix

--- bg.py
+++ bg.py
@@ -13,6 +13,7 @@ class BgTask(commands.Cog):
   
     async def user_progress(self):
         channel = self.bot.get_channel(451385630543577088)
+        #channel = self.bot.get_channel(573490564646043649)
         await channel.send('SnakeBot start daily background roster check for registered users')
         #Get list of {ally:code , user_id}
         while True:
@@ -50,15 +51,16 @@ class BgTask(commands.Cog):
     @commands.command(pass_context=True)
     @commands.has_any_role('Master') # User need this role to run command (can have multiple)
     async def startbg(self, ctx):
-        channel = self.bot.get_channel(451385630543577088)
+        
         self.bg_task = self.bot.loop.create_task(self.user_progress())
-        await channel.send('Daily roster saves - STARTED')
+        #await channel.send('Daily roster saves - STARTED')
 
 
     @commands.command(pass_context=True)
     @commands.has_any_role('Master') # User need this role to run command (can have multiple)
     async def stopbg(self, ctx):
         channel = self.bot.get_channel(451385630543577088)
+        #channel = self.bot.get_channel(573490564646043649)
         self.bg_task.cancel()
         await channel.send('Task1 cycles - STOPPED')
 
