b0ed2e338514d04d8291ecf509281b2938426a68
deesnow <janos.tarjanyi@gmail.com>
2019-08-26 09:43:25 +0200

    little handler tweek

--- swgoh_handler.py
+++ swgoh_handler.py
@@ -4,18 +4,18 @@ import logging
 
 class Swgoh():
     def __init__(self,):
-        self.base_url = "https://swgoh.gg/api/player/"
+        self.base_url = "https://swgoh.gg/api/"
         self.logger = logging.getLogger(__name__)
         self.payload = ""
         self.header = {
-            'User-Agent': "SnakeBot/0.0.1",
+            'User-Agent': "SnakeBot/1.2",
             'Accept': "*/*",
             'Connection': "keep-alive"
         }
     
     def swgoh_getuser(self, allycode):
         self.allycode = allycode
-        self.url = self.base_url + str(self.allycode)
+        self.url = self.base_url + 'player/' + str(self.allycode)
 
 
         try:
@@ -29,6 +29,21 @@ class Swgoh():
 
             return None
 
+    def swgoh_getmods(self, allycode):
+        self.allycode = allycode
+        self.url = self.base_url + 'players/' + str(self.allycode) + '/mods'
+
+
+        try:
+            self.response = requests.request("GET", self.url, data=self.payload)
+            self.logger.info('Response 200 OK')
+
+            return json.loads(self.response.text)
+            
+        except Exception as error:
+            self.logger.exception('GET url is failed - [{}]'.format(error))
+
+            return None
     
 
 
