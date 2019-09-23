bc5058c14ad451f422df6afba7f33b9a25fcef22
deesnow <janos.tarjanyi@gmail.com>
2019-09-23 16:34:49 +0200

    add cmdTest

---
+++ cmdTest.py
@@ -0,0 +1,45 @@
+from discord.ext import commands
+import asyncio
+import logging
+import settings
+from datetime import datetime 
+
+
+class HelpApiTest(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger(__name__)
+        self.logger.info('Init Test COG')
+
+#-------------------------------------------------------------------------------
+
+    async def getGuild(self, taskNo):
+        
+        self.starttime = datetime.now()
+        self.logger.info(f"No{taskNo} task is started.")
+        await asyncio.sleep(10)
+        self.logger.info(f"No{taskNo} task is completed")
+        return taskNo + 10
+
+
+
+
+
+    @commands.command(pass_context=True)
+    # 
+    async def test(self, ctx):
+        self.ctx = ctx
+        await self.ctx.message.add_reaction("🐍")
+
+        myloop = asyncio.get_event_loop()
+
+        tasklist = await asyncio.gather(self.getGuild(1), self.getGuild(2), self.getGuild(3))
+        
+        self.logger.info(f"{tasklist}")        
+        self.logger.info(f"Just something to print")
+        
+
+
+#-------------------------------------------------------------------------------
+def setup(bot):
+    bot.add_cog(HelpApiTest(bot))
